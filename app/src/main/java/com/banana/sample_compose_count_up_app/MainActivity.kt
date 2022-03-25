db30d461d650e9087fccb8bc946608ee68d1a6b2
bananarizumu <ne250068@gmail.com>
2022-03-25 11:15:13 +0900

    create count up screen layout

--- app/src/main/java/com/banana/sample_compose_count_up_app/MainActivity.kt
+++ app/src/main/java/com/banana/sample_compose_count_up_app/MainActivity.kt
@@ -3,36 +3,75 @@ package com.banana.sample_compose_count_up_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.banana.sample_compose_count_up_app.ui.theme.SamplecomposecountupappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SamplecomposecountupappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Counter()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Counter() {
+    Scaffold(
+        // ①トップバー
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        textAlign = TextAlign.Center,
+                        text = "Count Up App",
+                    )
+                },
+            )
+        },
+        // ③右下のアクションボタン
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /* ... */ }) {
+                /* FAB content */
+                Icon(Icons.Filled.Add, "Add")
+            }
+        }
+    ) {}
+
+    // ②画面中央のカウントを表示するテキスト
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "You have pushed the button this many times:",
+            fontSize = 12.sp
+        )
+        Text(
+            text = "0",
+            modifier = Modifier.padding(8.dp),
+            fontSize = 24.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    SamplecomposecountupappTheme {
-        Greeting("Android")
-    }
+fun CounterPreview() {
+    Counter()
 }
